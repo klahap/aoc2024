843b72a9097c3e28c6b5aa2c5eca68d6b41e382a
Klaus Happacher <k.happacher@gmail.com>
2024-12-01 22:32:44 +0100

    init

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    kotlin("jvm") version "2.0.21"
+}
+
+group = "io.github.klahap"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+}
+
+kotlin {
+    jvmToolchain(21)
+}
