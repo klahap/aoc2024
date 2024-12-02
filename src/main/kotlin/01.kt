0255959505ed2a0baa0760c12127f28092fdae03
Klaus Happacher <k.happacher@gmail.com>
2024-12-02 09:56:50 +0100

    01_2 and 02_2

--- src/main/kotlin/01.kt
+++ src/main/kotlin/01.kt
@@ -2,17 +2,31 @@ package io.github.klahap
 
 import kotlin.math.absoluteValue
 
-fun main() = fileReader("01.txt").lineSequence()
-    .map {
-        val e1 = it.substringBefore(' ').toInt()
-        val e2 = it.substringAfterLast(' ').toInt()
-        e1 to e2
+private val parsedData
+    get() = fileReader("01.txt").lineSequence()
+        .map {
+            val e1 = it.substringBefore(' ').toInt()
+            val e2 = it.substringAfterLast(' ').toInt()
+            e1 to e2
+        }
+        .toList()
+
+private fun task1() = parsedData.let { data ->
+    val column1 = data.map { it.first }.sorted()
+    val column2 = data.map { it.second }.sorted()
+    column1.zip(column2)
+}.sumOf { (e1, e2) -> (e2 - e1).absoluteValue }
+
+private fun task2() = parsedData.let { data ->
+    val column1 = data.map { it.first }.groupingBy { it }.eachCount()
+    val column2 = data.map { it.second }.groupingBy { it }.eachCount()
+    column1.entries.sumOf { (x, count1) ->
+        val count2 = column2[x] ?: 0
+        x * count1 * count2
     }
-    .toList()
-    .let { data ->
-        val column1 = data.map { it.first }.sorted()
-        val column2 = data.map { it.second }.sorted()
-        column1.zip(column2)
-    }
-    .sumOf { (e1, e2) -> (e2 - e1).absoluteValue }
-    .let(::println)
+}
+
+fun main() {
+    task1().let(::println)
+    task2().let(::println)
+}
--- src/main/kotlin/02.kt
+++ src/main/kotlin/02.kt
@@ -1,12 +1,31 @@
 package io.github.klahap
 
-fun main() = fileReader("02.txt").lineSequence()
-    .filter { line ->
-        val diffs = line
-            .split("\\s+".toRegex())
-            .map(String::toInt)
-            .windowed(2) { (a, b) -> b - a }
-        val isValidIncreasing = diffs.all { it in +1..+3 }
-        val isValidDecreasing = diffs.all { it in -3..-1 }
-        isValidIncreasing || isValidDecreasing
-    }.count().let(::println)
+import kotlin.collections.component1
+import kotlin.collections.component2
+
+
+private val parsedData
+    get() = fileReader("02.txt").lineSequence()
+        .map { it.split("\\s+".toRegex()).map(String::toInt) }
+
+private fun List<Int>.isValid(): Boolean {
+    val diffs = windowed(2) { (a, b) -> b - a }
+    val isValidIncreasing = diffs.all { it in +1..+3 }
+    val isValidDecreasing = diffs.all { it in -3..-1 }
+    return isValidIncreasing || isValidDecreasing
+}
+
+private fun task1() = parsedData.count { it.isValid() }
+
+private fun task2() = parsedData.count { line ->
+    sequence {
+        yield(line)
+        for (i in 0..<(line.size))
+            yield(line.toMutableList().apply { removeAt(i) })
+    }.any { it.isValid() }
+}
+
+fun main() {
+    task1().let(::println)
+    task2().let(::println)
+}
