9ea747559d597b86c2d5dd336b9370ed905a47ec
Klaus Happacher <k.happacher@gmail.com>
2024-12-12 17:35:42 +0100

    12 refactor

--- src/commonMain/kotlin/12.kt
+++ src/commonMain/kotlin/12.kt
@@ -1,35 +1,27 @@
 package io.github.klahap
 
+private typealias Region = ULong
+
 private val parsedData by lazy { fileReader("data/12.txt").lineSequence().toList().toCharMatrix() }
-
-private data class Region(
-    val area: Int = 0,
-    val perimeter: Int = 0,
-) {
-    val price get() = area * perimeter
-    operator fun plus(other: Region) = Region(area = area + other.area, perimeter = perimeter + other.perimeter)
-}
-
-private fun Sequence<Region>.sum() = fold(Region()) { a, b -> a + b }
-
 private val offsets = arrayOf(IntPos2D(1, 0), IntPos2D(0, 1), IntPos2D(-1, 0), IntPos2D(0, -1))
 
-private fun CharMatrix.getRegionsWithNeighbours(pos: IntPos2D, origin: Char): Sequence<Region> = sequence {
-    when (getOrNull(pos)) {
-        origin.lowercaseChar() -> Unit
-        origin -> {
-            set(pos, origin.lowercaseChar())
-            yield(Region(area = 1))
-            for (offset in offsets)
-                yieldAll(getRegionsWithNeighbours(pos + offset, origin = origin))
-        }
+private fun region(area: UInt = 0u, perimeter: UInt = 0u): Region = (area.toULong() shl 32) + perimeter
+private val Region.area: UInt get() = (this shr 32).toUInt()
+private val Region.perimeter: UInt get() = ((this shl 32) shr 32).toUInt()
+private val Region.price get() = area * perimeter
 
-        else -> yield(Region(perimeter = 1))
-    }
+
+private fun CharMatrix.getRegionsWithNeighbours(pos: IntPos2D, origin: Char): Region {
+    val c = getOrNull(pos)
+    if (c == origin.lowercaseChar()) return region()
+    if (c != origin) return region(perimeter = 1u)
+    set(pos, origin.lowercaseChar())
+    val neighbourResult = offsets.sumOf { getRegionsWithNeighbours(pos + it, origin = origin) }
+    return region(area = 1u) + neighbourResult
 }
 
-private fun CharMatrix.getRegionsWithNeighbours2(pos: IntPos2D, origin: Char): Sequence<Region> = sequence {
-    if (getOrNull(pos) != origin) return@sequence
+private fun CharMatrix.getRegionsWithNeighbours2(pos: IntPos2D, origin: Char): Region {
+    if (getOrNull(pos) != origin) return region()
     set(pos, origin.lowercaseChar())
     fun isNeighbour(offset: IntPos2D) = getOrNull(pos + offset)?.uppercaseChar() != origin
     val corners = offsets.count { offset ->
@@ -38,25 +30,24 @@ private fun CharMatrix.getRegionsWithNeighbours2(pos: IntPos2D, origin: Char): S
         val c = isNeighbour(offset + offset.rotate90()) // to check if it's an inner corner
         (a && b) || (!a && !b && c)
     }
-    yield(Region(area = 1, perimeter = corners))
-    for (offset in offsets)
-        yieldAll(getRegionsWithNeighbours2(pos + offset, origin = origin))
+    val neighbourResult = offsets.sumOf { getRegionsWithNeighbours2(pos + it, origin = origin) }
+    return region(area = 1u, perimeter = corners.toUInt()) + neighbourResult
 }
 
 data object Day12a : Task<Int>({
     parsedData.copy().run {
-        values().map { (pos, c) ->
-            if (c.isLowerCase()) return@map Region()
-            getRegionsWithNeighbours(pos, c).sum()
-        }.sumOf(Region::price)
+        values().sumOf { (pos, c) ->
+            if (c.isLowerCase()) return@sumOf 0u
+            getRegionsWithNeighbours(pos, c).price
+        }.toInt()
     }
 })
 
 data object Day12b : Task<Int>({
     parsedData.copy().run {
-        values().map { (pos, c) ->
-            if (c.isLowerCase()) return@map Region()
-            getRegionsWithNeighbours2(pos, c).sum()
-        }.sumOf(Region::price)
+        values().sumOf { (pos, c) ->
+            if (c.isLowerCase()) return@sumOf 0u
+            getRegionsWithNeighbours2(pos, c).price
+        }.toInt()
     }
 })
